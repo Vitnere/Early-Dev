e1143cc1fba5c37aa6f42e05d42abafda8ce8a17
Vitnere <nemanjakolar@gmail.com>
2015-10-21 16:26:08 +0200

    Begin development od third version of Portofolio :wq :q

--- dynamic/js/FadeInOut.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('div').mouseenter(function(){ //div name of element
-    $('div').fadeTo('fast',1);		//fast speed of fade away,opacity
-    });
-    $('div').mouseleave(function(){
-        $('div').fadeTo('fast',0.5);
-        }); 
-    });
