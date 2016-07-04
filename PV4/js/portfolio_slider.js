31a9e6db721208a7af40c648cf6f1180d646f394
Nemanja Kolar <nemanjakolar@gmail.com>
2016-07-04 12:10:34 +0200

    minor updates
    pictures optimization for web, grammatical corrections, links update, debugging

--- PV4/js/portfolio_slider.js
+++ PV4/js/portfolio_slider.js
@@ -7,147 +7,151 @@ $("#elastic_grid_demo").elastic_grid({
 	'expandingHeight': 500,
 	'items' :
 		[
-		{
-		'title' : 'Portofolio 3',
-		'description'   : 'Sajt razvijen u toku novembra 2015-e.Toolbox:HTML5,CSS3,Js/Jquery/Bootstrap',
+
+            {
+                'title' : 'myTodo',
+                'description'   : 'Moja prva web app.',
+                'thumbnail' : ['http://i.imgur.com/906L4mI.jpg','http://i.imgur.com/lkhRy4L.jpg','http://i.imgur.com/iqbTkL8.jpg',
+                    'http://i.imgur.com/x2SE4jn.jpg','http://i.imgur.com/frqBRsA.jpg','http://i.imgur.com/r90BlX5.jpg'],
+                'large' : ['http://i.imgur.com/sfp0PZ1.jpg','http://i.imgur.com/4HhnswW.jpg','http://i.imgur.com/CZkzHaa.jpg','http://i.imgur.com/1gZeqsU.jpg','http://i.imgur.com/PxfHJvI.jpg','http://i.imgur.com/ABZ9NjK.jpg'],
+                'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://mytodo.96.lt/' },
+                        { 'title':'GitHub', 'url':'https://github.com/Vitnere/mytodo'}
+                    ],
+                'tags'  : ['PHP','Sve']
+            },
+
+            {
+                'title' : 'Meetings',
+                'description'   : 'Moj prvi CMS, trenutno u razvoju. Racun za logovanje->Username:guest, sifra:12345678',
+                'thumbnail' : ['http://i.imgur.com/ugaXFF1.jpg','http://i.imgur.com/nt2GuSW.jpg','http://i.imgur.com/D1Y21P8.jpg','http://i.imgur.com/hHhnqfJ.jpg','http://i.imgur.com/7CtruK2.jpg','http://i.imgur.com/OoZsqtQ.jpg'],
+                'large' : ['http://i.imgur.com/6BRl4Js.jpg','http://i.imgur.com/uGxQ472.jpg','http://i.imgur.com/mmbX3yn.jpg','http://i.imgur.com/POPaahi.jpg','http://i.imgur.com/x9CyTj7.jpg','http://i.imgur.com/PgGfi1n.jpg'],
+                'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://meetings.pe.hu/' },
+                        { 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/meetings%20export.zip'}
+
+
+                    ],
+                'tags'  : ['PHP', 'Sve']
+            },
+
+            {
+        'title' : 'E-shop',
+        'description'   : 'E-commerce sajt razvijen u wordpressu',
+        'thumbnail' : ['http://i.imgur.com/TZckhqj.jpg','http://i.imgur.com/1JqDy15.jpg','http://i.imgur.com/YtYfZEr.jpg','http://i.imgur.com/dt3AZb2.jpg'],
+        'large' : ['http://i.imgur.com/fafkm5T.jpg','http://i.imgur.com/iUlJw1u.jpg','http://i.imgur.com/4dwVBYQ.jpg','http://i.imgur.com/EAvUN5J.jpg'],
+        'button_list'   :
+            [
+                { 'title':'Demo', 'url' : 'http://eshoppd.pe.hu/' },
+                
+            ],
+        'tags'  : ['Wordpress','Sve']
+        },
+
+		    {
+		'title' : 'Studentska Unija Prijedor',
+		'description'   : 'Rad za klijenta',
+		'thumbnail' : ['http://i.imgur.com/TsU0qJ8.jpg','http://i.imgur.com/CULgE6O.jpg','http://i.imgur.com/YxReD1F.jpg','http://i.imgur.com/R1pS2oA.jpg'],
+		'large' : ['http://i.imgur.com/MEq9zSX.jpg','http://i.imgur.com/y7XOj7a.jpg','http://i.imgur.com/R1pS2oA.jpg','http://i.imgur.com/7XSVI7h.jpg'],
+		'button_list'   :
+		[
+		{ 'title':'Demo', 'url' : 'http://mladjopaukovic.byethost7.com/wp/' },
+		],
+		'tags'  : ['Wordpress','Sve']
+		},
+
+		    {
+		'title' : 'First Portofolio',
+		'description'   : 'Sajt razvijen u toku novembra 2015-e.',
 		'thumbnail' : ['http://i.imgur.com/EXeawWR.jpg', 'http://i.imgur.com/g4IqxXM.jpg', 'http://i.imgur.com/m0X1Fdk.jpg', 'http://i.imgur.com/A6vRVkN.jpg', 'http://i.imgur.com/GJDMR7o.jpg'],
 		'large' : ['http://i.imgur.com/DBg4trk.jpg','http://i.imgur.com/OAdJGEt.jpg','http://i.imgur.com/FK4B5MI.jpg','http://i.imgur.com/DmPmbl3.jpg','http://i.imgur.com/wmMXpPE.jpg'],
 		'button_list'   :
 		[
 		{ 'title':'Demo', 'url' : 'http://1989.bitballoon.com/' },
-		{ 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/PortofolioV3.rar'}
+		{ 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/PortofolioV3.rar'},
+            { 'title':'GitHub', 'url':' https://github.com/Vitnere/Early-Dev/tree/master/pv3'}
 		],
-		'tags'  : ['Web','Sve']
+		'tags'  : ['Frontend','Sve']
 		},
 
-		{
+		    {
 		'title' : 'Test 23',
-		'description'   : 'Sajt urađen za tri sata po zahjevu firme na konkursu za posao.Download source coda sa zadatkom na dugme download',
+		'description'   : 'Sajt urađen za tri sata po zahjevu firme na konkursu za posao ',
 		'thumbnail' : ['http://i.imgur.com/bp0EPKW.jpg','http://i.imgur.com/faGZDNJ.jpg','http://i.imgur.com/JQk5xqy.jpg','http://i.imgur.com/wVk0WFS.jpg'],
 		'large' : ['http://i.imgur.com/lS2FRIH.jpg','http://i.imgur.com/MABBHuS.jpg','http://i.imgur.com/QuhpECE.jpg','http://i.imgur.com/CvKDh2Y.jpg'],
 		'button_list'   :
+			[
+				{ 'title':'Demo', 'url' : 'http://test23.bitballoon.com/' },
+				{ 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/test23.rar'}
+			],
+		'tags'  : ['Frontend','Sve']
+		},
+
+		{
+		'title' : '',
+		'description'   : '',
+		'thumbnail' : [''],
+		'large' : [''],
+		'button_list'   :
 		[
 		{ 'title':'Demo', 'url' : '' },
-		{ 'title':'Download', 'url':'http://#'}
+		{ 'title':'Download', 'url':''}
 		],
-		'tags'  : ['Web','Sve']
+		'tags'  : ['','']
 		},
 
 		{
-		'title' : 'Portofolio 2',
-		'description'   : 'Raden u maju 2013-e,HTML/CSS/Js',
-		'thumbnail' : ['http://i.imgur.com/xUx9hjG.jpg','http://i.imgur.com/3VCZVHt.jpg','http://i.imgur.com/u6u8eHh.jpg','http://i.imgur.com/ZDaks8V.jpg','http://i.imgur.com/GVAo2pU.jpg'],
-		'large' : ['http://i.imgur.com/L2NXekv.jpg','http://i.imgur.com/7WHum9N.jpg','http://i.imgur.com/A0XjMYZ.jpg','http://i.imgur.com/DFlB4zL.jpg','http://i.imgur.com/Nrlt0Rs.jpg'],
-		'button_list'   :
-		[
-		{ 'title':'Demo', 'url' : 'http://test23.bitballoon.com/' },
-		{ 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/test23.rar'}
-		],
-		'tags'  : ['Web', 'Sve']
-		},
-
-		{
-		'title' : 'Brodway',
-		'description'   : ' Razvijen u septembru 2015-e.Codecademy primjer za vjezbu:HTML,CSS',
-		'thumbnail' : ['http://i.imgur.com/l3DI6HZ.jpg','http://i.imgur.com/K9r0H6K.jpg'],
-		'large' : ['http://i.imgur.com/TVeJ38K.jpg','http://i.imgur.com/TVeJ38K.jpg'],
-		'button_list'   :
-		[
-		{ 'title':'Demo', 'url' : 'http://p5brodway.bitballoon.com/' },
-		{ 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/Brodway.rar'}
-		],
-		'tags'  : ['Web','Sve']
-		},
-
-		{
-		'title' : 'Airbnb',
-		'description'   : 'Razvijen u septembru 2015-e.Codecademy primjer za vjezbu:HTML,CSS,Bootstrap',
-		'thumbnail' : ['http://i.imgur.com/YbtInk0.jpg','http://i.imgur.com/9tkL2uZ.jpg'],
-		'large' : ['http://i.imgur.com/3zzz7BL.jpg','http://i.imgur.com/rXypvWh.jpg'],
-		'button_list'   :
-		[
-		{ 'title':'Demo', 'url' : 'http://p4airbnb.bitballoon.com/' },
-		{ 'title':'Download', 'url':'https://dl.dropboxusercontent.com/u/22380934/Airbnb.rar'}
-		],
-		'tags'  : ['Web','Sve']
-		},
-
-		{
-		'title' : 'Preporuka E-info',
+		'title' : '',
 		'description'   : '',
-		'thumbnail' : ['http://i.imgur.com/EUA7CHn.jpg'],
-		'large' : ['http://i.imgur.com/7bRvHs0.jpg'],
+		'thumbnail' : [''],
+		'large' : [''],
 		'button_list'   :
 		[
-		{ 'title':'Demo', 'url' : 'http://i.imgur.com/O3dkzl9.png' },
-		{ 'title':'Download', 'url':'http://i.imgur.com/O3dkzl9.png'}
+		{ 'title':'Demo', 'url' : '' },
+		{ 'title':'Download', 'url':''}
 		],
-		'tags'  : ['Preporuke','Sve']
+		'tags'  : ['','']
 		},
 
 		{
-		'title' : 'Preporuka Int-Ware',
+		'title' : '',
 		'description'   : '',
-		'thumbnail' : ['http://i.imgur.com/oyKpoTd.jpg'],
-		'large' : ['http://i.imgur.com/wgeSmHD.jpg'],
+		'thumbnail' : [''],
+		'large' : [''],
 		'button_list'   :
 		[
-		{ 'title':'Demo', 'url' : 'http://i.imgur.com/nPYwRan.jpg' },
-		{ 'title':'Download', 'url':'http://i.imgur.com/oyKpoTd.jpg'}
+		{ 'title':'Demo', 'url' : '' },
+		{ 'title':'Download', 'url':''}
 		],
-		'tags'  : ['Preporuke','Sve']
+		'tags'  : ['','']
 		},
 
 		{
-		'title' : 'Preporuka Docent,dr Dragan Matić',
-		'description'   : 'Preporuka Docent,dr Dragan Matić,PMF Banja Luka predavac na kursu "Osnove programiranja sa C jezikom"',
-		'thumbnail' : ['http://i.imgur.com/jDHMq8z.jpg'],
-		'large' : ['http://i.imgur.com/RDDus9o.jpg'],
+		'title' : '',
+		'description'   : '',
+		'thumbnail' : [''],
+		'large' : [''],
 		'button_list'   :
 		[
-		{ 'title':'Demo', 'url' : 'http://i.imgur.com/ycQw7fL.jpg' },
-		{ 'title':'Download', 'url':'http://i.imgur.com/ycQw7fL.jpg'}
+		{ 'title':'Demo', 'url' : '' },
+		{ 'title':'Download', 'url':''}
 		],
-		'tags'  : ['Preporuke','Sve']
+		'tags'  : ['','']
 		},
 
 		{
-		'title' : 'Preporuka Docent,mr Miodrag Manojlović',
-		'description'   : 'Preporuka docent,mr Miodrag Manojlović, profesor, Faculty of Information Technology and Design',
-		'thumbnail' : ['http://i.imgur.com/vLJnvQw.jpg'],
-		'large' : ['http://i.imgur.com/yRZ2y0w.jpg'],
+		'title' : '',
+		'description'   : '',
+		'thumbnail' : [''],
+		'large' : [''],
 		'button_list'   :
 		[
-		{ 'title':'Demo', 'url' : 'http://i.imgur.com/TYXzhIw.png' },
-		{ 'title':'Download', 'url':'http://i.imgur.com/TYXzhIw.png'}
+		{ 'title':'Demo', 'url' : '' },
+		{ 'title':'Download', 'url':''}
 		],
-		'tags'  : ['Preporuke','Sve']
-		},
-
-		{
-		'title' : '"Osnove programiranja sa C jezikom"',
-		'description'   : 'Certifikat sa kursa "Osnove programiranja sa C jezikom"',
-		'thumbnail' : ['http://i.imgur.com/ReDxkGJ.jpg'],
-		'large' : ['http://i.imgur.com/R4qQnFt.jpg'],
-		'button_list'   :
-		[
-		{ 'title':'Demo', 'url' : 'http://i.imgur.com/xJ2yMJz.png' },
-		{ 'title':'Download', 'url':'http://i.imgur.com/xJ2yMJz.png'}
-		],
-		'tags'  : ['Certifikati','Sve']
-		},
-
-		{
-		'title' : 'Obuka u naprednim grafičkim softverima "Adobe Illustrator" i "Adobe InDesign"',
-		'description'   : 'Certifikat sa kursa Obuka u naprednim grafičkim softverima "Adobe Illustrator" i "Adobe InDesign"',
-		'thumbnail' : ['http://i.imgur.com/qspwlB5.jpg'],
-		'large' : ['http://i.imgur.com/DATPNxE.jpg'],
-		'button_list'   :
-		[
-		{ 'title':'Demo', 'url' : 'http://i.imgur.com/jz33usv.jpg' },
-		{ 'title':'Download', 'url':'http://i.imgur.com/jz33usv.jpg'}
-		],
-		'tags'  : ['Certifikati','Sve']
+		'tags'  : ['','']
 		},
 
 		{
